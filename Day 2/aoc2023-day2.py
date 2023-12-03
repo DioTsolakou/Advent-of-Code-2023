b3a5290f2c2fbcc4ea9e708bdafd361b64a2d92e
Diogenis Tsolakou <dio.tsolakou@hotmail.com>
2023-12-03 16:10:44 +0200

    Day 2 part 2

--- Day 2/aoc2023-day2.py
+++ Day 2/aoc2023-day2.py
@@ -1,31 +1,51 @@
 import os
-import re
+from math import prod
 
 def check_set_values(dictionary):
     ret = True
     max_red, max_green, max_blue = 12, 13, 14
-    if ('red' in dictionary and dictionary['red'] > max_red) or ('blue' in dictionary and dictionary['blue'] > max_blue) or ('green' in dictionary and dictionary['green'] > max_green):
+    if (dictionary['red'] > max_red) or (dictionary['blue'] > max_blue) or (dictionary['green'] > max_green):
         ret = False
     return ret
 
+def get_max(game_maxes, dictionary):
+    for key in dictionary.keys():
+        if dictionary[key] > game_maxes[key]:
+            game_maxes[key] = dictionary[key]
 
-def possible_games(games):
-    
+    return game_maxes
+
+
+def possible_games(games, p2 = False):
     total_sum = 0
 
     for i in range(len(games)):
         possible_game = True
         game = games[i]
+        game_maxes = {'red': 0, 'blue': 0, 'green': 0}
         for set in game.split('; '):
             dictionary = dict((y.strip(), int(x.strip()))
              for x, y in (element.split(' ') 
              for element in set.split(', ')))
             
-            if not check_set_values(dictionary):
+            if 'red' not in dictionary:
+                dictionary['red'] = 0
+            if 'blue' not in dictionary:
+                dictionary['blue'] = 0
+            if 'green' not in dictionary:
+                dictionary['green'] = 0
+            
+            if p2:
+               game_maxes = get_max(game_maxes, dictionary)
+            elif not check_set_values(dictionary):
                 possible_game = False
                 break
+                
 
-        if possible_game:
+        if p2:
+            game_maxes_val_list = game_maxes.values()
+            total_sum += prod(game_maxes_val_list)
+        elif possible_game:
             total_sum += i + 1
     
     return total_sum
@@ -38,8 +58,11 @@ def main():
     input = open(full_path, "r")
     input_array = [line.strip()[line.index(':') + 2:] for line in input]
 
-    result = possible_games(input_array)
-    print(result)
+    result1 = possible_games(input_array)
+    result2 = possible_games(input_array, True)
+
+    print(result1)
+    print(result2)
 
 if __name__ == '__main__':
     main()
