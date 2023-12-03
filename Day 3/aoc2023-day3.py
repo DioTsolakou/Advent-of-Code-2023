a5721e0eca29f81b5d580a5b08b7581ccab55f61
Diogenis Tsolakou <dio.tsolakou@hotmail.com>
2023-12-03 21:42:52 +0200

    Day 3 part 2

--- Day 3/aoc2023-day3.py
+++ Day 3/aoc2023-day3.py
@@ -1,5 +1,6 @@
 import os
 import re
+from math import prod
 
 def check_adjacent_symbols(curr_line, input_array, i, special_chars, total_sum):
     empty_line = '.' * len(curr_line)
@@ -59,6 +60,39 @@ def sum_part_numbers(input_array, special_chars):
         
     return total_sum
 
+def check_gears(curr_line, input_array, i, gear_char, total_sum):
+    empty_line = '.' * len(curr_line)
+    input_array_with_empy_lines = [empty_line] + input_array + [empty_line]
+
+    for gear in re.finditer(gear_char, curr_line):
+        gear_start, gear_end = gear.span()
+
+        # not sure why I copied this from part 1, cost me too much time trying to find why total_sum was wrong...
+        #gear_start, gear_end = max(gear_start - 1, 0), min(gear_end + 1, len(curr_line))
+
+        adjacent_nums = []
+
+        for row in input_array_with_empy_lines[i:i+3]:
+            for num in re.finditer(r'\d+', row):
+                num_start, num_end = num.span()
+                
+                if gear_start in range(num_start - 1, num_end + 1):
+                    adjacent_nums.append(int(num.group(0)))
+        
+        if len(adjacent_nums) == 2:
+            total_sum += prod(adjacent_nums)
+
+    return total_sum
+
+def sum_gear_ratios(input_array, gear_char):
+    total_sum = 0
+
+    for i in range(len(input_array)):
+        curr_line = input_array[i]
+        total_sum = check_gears(curr_line, input_array, i, gear_char, total_sum)
+    
+    return total_sum
+
 def main():
     current_path = os.path.dirname(__file__)
     input_path = "day3-input.txt"
@@ -66,9 +100,13 @@ def main():
     input = open(full_path, "r")
     input_array = [line.strip() for line in input]
     special_chars = re.compile(r'[^.0-9]')
+    gear_char = re.compile(r'\*')
 
-    result = sum_part_numbers(input_array, special_chars)
-    print(result)
+    result1 = sum_part_numbers(input_array, special_chars)
+    result2 = sum_gear_ratios(input_array, gear_char)
+
+    print(result1)
+    print(result2)
 
 if __name__ == '__main__':
     main()
