3f7d82e6a32399ea21c0be29cb1e71bf8e781d0c
Diogenis Tsolakou <dio.tsolakou@hotmail.com>
2023-12-03 03:42:50 +0200

    Day 1 part 2 answer, brain-breaker..

--- Day 1/aoc2023-day1.py
+++ Day 1/aoc2023-day1.py
@@ -16,8 +16,38 @@ def part1(input_array):
 
     return total_sum
 
-""" def part2(input_array):
-    return input_array """
+def part2(input_array):
+    total_sum = 0
+    num_spelling = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }
+
+    num_words_regex = re.compile(r"(?=(\d|" + "|".join(num_spelling) + r"))")
+
+    for j in range(len(input_array)):
+        line = input_array[j]
+        found = re.findall(num_words_regex, line)
+        nums = [found[0], found[len(found) - 1]]
+
+        for i in range(len(nums)):
+            num = nums[i]
+            if num.isdigit():
+                nums[i] = num
+            else:
+                nums[i] = num_spelling.get(num)
+        
+        total_sum += int(''.join(nums))
+
+    return total_sum
 
 def main():
     current_path = os.path.dirname(__file__)
@@ -27,10 +57,10 @@ def main():
     input_array = [line.rstrip() for line in input]
 
     part1_answer = part1(input_array)
-    """ part1_answer = part2(input_array) """
-    
+    part2_answer = part2(input_array)
     
     print(part1_answer)
+    print(part2_answer)
 
 if __name__ == '__main__':
     main()
