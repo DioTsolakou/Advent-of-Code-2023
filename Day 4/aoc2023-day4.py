61c88b6fd74d8da3c441ce757aeb17e968f3226d
Diogenis Tsolakou <dio.tsolakou@hotmail.com>
2023-12-04 14:11:09 +0200

    Day 4 part 2

--- Day 4/aoc2023-day4.py
+++ Day 4/aoc2023-day4.py
@@ -1,17 +1,29 @@
 import os
+from collections import defaultdict
 
-def get_total_points(input_array):
+def get_total_points(input_array, p2=False):
     total_sum = 0
+    input_len = len(input_array)
 
-    for i in range(len(input_array)):
+    total_cards = []
+    total_cards = defaultdict(lambda: 0 * len(input_array))
+
+    for i in range(input_len):
+        total_cards[i] += 1
         curr_card = input_array[i]
         winning_nums, our_nums = curr_card.split('|')
         winning_nums, our_nums = winning_nums.split(), our_nums.split()
 
         card_occurrences = sum(x in winning_nums for x in our_nums)
 
-        if card_occurrences:
+        if p2 and card_occurrences:
+            for j in range(min(i + 1, input_len), min(i + (1*card_occurrences) + 1, input_len)):
+                total_cards[j] += total_cards[i]
+        elif card_occurrences:
             total_sum += 1 * pow(2, card_occurrences - 1)
+    
+    if p2:
+        total_sum = sum(total_cards.values())
 
     return total_sum
 
@@ -22,8 +34,11 @@ def main():
     input = open(full_path, "r")
     input_array = [line.strip()[line.index(':') + 2:] for line in input]
 
-    result = get_total_points(input_array)
-    print(result)
+    result1 = get_total_points(input_array)
+    result2 = get_total_points(input_array, True)
+
+    print(result1)
+    print(result2)
 
 
 if __name__ == '__main__':
